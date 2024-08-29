7d48978f5f0849fb53f148793e43b9bd1001f716
myhousevpn <myhousevpn@gmail.com>
2024-08-29 16:52:47 +0200

    Debugg=False

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ def vpn():
     return render_template('vpn.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=False)
